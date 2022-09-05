dceaa81f928568a3ee6717295c9e55594eb0e009
parksol <thfdl0317@naver.com>
2022-09-05 14:49:40 +0900

    feat :bodyParser설치 서버전달, write.html생성 및 코드 추가

--- todoList/server.js
+++ todoList/server.js
@@ -1,5 +1,7 @@
 const express = require('express'); //설치한 라이브러리를 설치해줘~
 const app = express(); //첨부한 라이브러리를 이용하여 새로운 객체를 만듬
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended : true}));
 
 app.listen(8080, function(){
     console.log('test 8080')
@@ -8,5 +10,15 @@ app.listen(8080, function(){
 //서버를 띄우기 위한 기본 문법(서버 오픈하는 문법)
 
 app.get('/', function(req, res){
-    res.sendfile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/write', function(req, res){
+    res.sendFile(__dirname + '/write.html')
+});
+
+app.post('/add', function(req, res){
+    res.send('전송완료');
+    console.log(req.body.title);
+    console.log(req.body.date);
 });
