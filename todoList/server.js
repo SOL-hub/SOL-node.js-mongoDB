fb249d78fe423fe95ced8cb4ae166c6d97eebe9d
parksol <thfdl0317@naver.com>
2022-09-05 15:30:34 +0900

    feat : MongoDB 셋팅,  server.js에 코드작성하여 MongoDB 와 연결

--- todoList/server.js
+++ todoList/server.js
@@ -1,11 +1,17 @@
 const express = require('express'); //설치한 라이브러리를 설치해줘~
 const app = express(); //첨부한 라이브러리를 이용하여 새로운 객체를 만듬
+
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended : true}));
 
-app.listen(8080, function(){
-    console.log('test 8080')
-});//서버어디다 열지 괄호안에 적어주기
+const MongoClient = require('mongodb').MongoClient;
+MongoClient.connect('mongodb+srv://parksol:<password>@cluster0.sxmgir1.mongodb.net/?retryWrites=true&w=majority', function(error, client){
+
+    app.listen(8080, function(){
+        console.log('test 8080')
+    });
+});
+
 
 //서버를 띄우기 위한 기본 문법(서버 오픈하는 문법)
 
