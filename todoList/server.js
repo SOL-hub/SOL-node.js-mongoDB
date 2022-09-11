f64f77c9dfbd187b864f15a4c9e6f0cafa4bd840
parksol <thfdl0317@naver.com>
2022-09-11 10:56:55 +0900

    feat : 상세페이지를 만들기 (URL parameter)

--- todoList/server.js
+++ todoList/server.js
@@ -74,3 +74,10 @@ app.delete('/delete', function(req, res){
         res.status(200).send({message : '성공했습니다.'});
     });
 });
+
+app.get('/detail/:id', function(req, res){
+    db.collection('post').findOne({_id: parseInt(req.params.id)}, function(error, result){
+        console.log(result);
+        res.render('detail.ejs', {data : result});
+    })
+});
